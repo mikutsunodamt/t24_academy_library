b4c163a194d9ba576af1b76e6b4dacc8e7bedeca
mikutsunodamt <miku.tsunoda@metateam.co.jp>
2024-05-14 12:10:45 +0900

    貸出編集機能追加

--- src/main/java/jp/co/metateam/library/controller/RentalManageController.java
+++ src/main/java/jp/co/metateam/library/controller/RentalManageController.java
@@ -12,15 +12,21 @@ import lombok.extern.log4j.Log4j2;
  
 import jp.co.metateam.library.model.RentalManage;
 import jp.co.metateam.library.model.RentalManageDto;
+
 import java.util.List;
 import jp.co.metateam.library.model.Account;
 import jp.co.metateam.library.model.Stock;
 import jp.co.metateam.library.values.RentalStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+ 
  
 /**
 * 貸出管理関連クラスß
@@ -94,5 +100,86 @@ public class RentalManageController {
             return "redirect:/rental/add";
         } 
     } 
+    @GetMapping("/rental/{id}/edit")
+    public String edit(@PathVariable("id") String id, Model model) {
+        
+        List<Account> accounts = this.accountService.findAll();
+        List <Stock> stockList = this.stockService.findStockAvailableAll();
+
+        model.addAttribute("accounts", accounts);
+        model.addAttribute("stockList", stockList);
+        model.addAttribute("rentalStatus", RentalStatus.values());
+ 
+
+        if (!model.containsAttribute("rentalManageDto")){
+            RentalManage rentalManage = this.rentalManageService.findById(Long.valueOf(id));
+
+            RentalManageDto rentalManageDto = new RentalManageDto();
+
+            rentalManageDto.setId(rentalManage.getId());
+            rentalManageDto.setEmployeeId(rentalManage.getAccount().getEmployeeId());
+            rentalManageDto.setExpectedRentalOn(rentalManage.getExpectedRentalOn());
+            rentalManageDto.setExpectedReturnOn(rentalManage.getExpectedReturnOn());
+            rentalManageDto.setStockId(rentalManage.getStock().getId());
+            rentalManageDto.setStatus(rentalManage.getStatus());
+
+            model.addAttribute("rentalManageDto", rentalManageDto);
+
+
+
+        }
+        
+        
+        return "rental/edit";
+    }
+    
+    @PostMapping("/rental/{id}/edit")
+    public String update(@PathVariable("id") String id,@Valid @ModelAttribute RentalManageDto rentalManageDto, BindingResult result, RedirectAttributes ra, Model model) {
+        try {
+            //変更前の貸出情報をとってくる
+            RentalManage rentalManage = this.rentalManageService.findById(Long.valueOf(id));
+
+            //変更前の貸出ステータスを用いてバリデーションチェック
+            String errMsgOfStatus = rentalManageDto.validateStatus(rentalManage.getStatus());
+
+    
+            
+            
+            if(errMsgOfStatus != null){
+                 result.addError(new FieldError("rentalManageDto", "status", errMsgOfStatus));
+            }
+            if (result.hasErrors()) {
+                throw new Exception("Validation error.");
+            }
+            
+            
+           
+             // 更新処理
+            this.rentalManageService.update(Long.valueOf(id), rentalManageDto);
+ 
+            return "redirect:/rental/index";
+            
+        } catch (Exception e) {
+            log.error(e.getMessage());
+
+            ra.addFlashAttribute("rentalManageDto", rentalManageDto);
+            ra.addFlashAttribute("org.springframework.validation.BindingResult.stockDto", result);
+
+            
+
+            List<Account> accounts = this.accountService.findAll();
+            List <Stock> stockList = this.stockService.findStockAvailableAll();
+
+            model.addAttribute("accounts", accounts);
+            model.addAttribute("stockList", stockList);
+            model.addAttribute("rentalStatus", RentalStatus.values());
+            
+            return "rental/edit";
+        } 
+    }
 }
+
+
+
+
  
--- src/main/java/jp/co/metateam/library/model/RentalManageDto.java
+++ src/main/java/jp/co/metateam/library/model/RentalManageDto.java
@@ -3,18 +3,27 @@ package jp.co.metateam.library.model;
 import java.sql.Timestamp;
 import java.util.Date;
 
+import javax.print.CancelablePrintJob;
+
 import org.springframework.format.annotation.DateTimeFormat;
 
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jp.co.metateam.library.values.RentalStatus;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Optional;
+
+
 
 /**
  * 貸出管理DTO
  */
 @Getter
 @Setter
+
 public class RentalManageDto {
 
     private Long id;
@@ -45,4 +54,58 @@ public class RentalManageDto {
     private Stock stock;
 
     private Account account;
+
+    //日付妥当性（返却予定日は貸出予定日より遅く）
+    @AssertTrue(message = "返却予定日は貸出予定日より後の日付を入力してください")
+    public boolean isDateValid(){
+        if(expectedReturnOn == null || expectedRentalOn == null){
+            return true;
+        }
+        return expectedReturnOn.after(expectedRentalOn);
+    }
+
+    
+    
+    //貸出ステータスのバリデーション
+
+    public String validateStatus (Integer prevStatus){
+        if(prevStatus == RentalStatus.RENT_WAIT.getValue() && this.status == RentalStatus.RETURNED.getValue()){
+            return("貸出ステータスは貸出待ちから返却済みにできません");
+        }else if (prevStatus == RentalStatus.RENTAlING.getValue() && this.status == RentalStatus.RENT_WAIT.getValue()){
+            return("貸出ステータスは貸出中から貸出待ちにできません");
+        }else if (prevStatus == RentalStatus.RENTAlING.getValue() && this.status == RentalStatus.CANCELED.getValue()){
+            return("貸出ステータスは貸出中からキャンセルにできません");
+        }else if (prevStatus == RentalStatus.RETURNED.getValue() && this.status == RentalStatus.RENT_WAIT.getValue()){
+            return("貸出ステータスは返却済みから貸出待ちにできません");
+        }else if (prevStatus == RentalStatus.RETURNED.getValue() && this.status == RentalStatus.RENTAlING.getValue()){
+            return("貸出ステータスは返却済みから貸出中にできません");
+        }else if (prevStatus == RentalStatus.RETURNED.getValue() && this.status == RentalStatus.CANCELED.getValue()){
+            return("貸出ステータスは返却済みからキャンセルにできません");
+        }else if (prevStatus == RentalStatus.CANCELED.getValue() && this.status == RentalStatus.RENT_WAIT.getValue()){
+            return("貸出ステータスはキャンセルから貸出待ちにできません");
+        }else if (prevStatus == RentalStatus.CANCELED.getValue() && this.status == RentalStatus.RENTAlING.getValue()){
+            return("貸出ステータスはキャンセルから貸出中にできません");
+        }else if (prevStatus == RentalStatus.CANCELED.getValue() && this.status == RentalStatus.RETURNED.getValue()){
+            return("貸出ステータスはキャンセルから返却済みにできません");
+        }
+
+        return null;
+
+
+
+
+
+
+
+        
+    }
+
+
+
+
+
+
 }
+
+
+
